b1bfe402513edf5cc82dea6f72353062baa9257b
viniciussilva512 <marvosvinicius128@gmail.com>
2023-10-11 11:39:41 -0300

    adicionei atividade03

---
+++ atividade03.py
@@ -0,0 +1,14 @@
+from time import sleep 
+nome1 = "vinicius"
+nome2 = "ruan"
+nome3 = "cabeludo"
+print('alunos DS1°')
+sleep(1)
+print(nome1)
+sleep(5)
+print(nome2)
+sleep(5)
+print(nome3)
+agradecimento = "============"
+for obrigado in agradecimento:
+    print(agradecimento)
